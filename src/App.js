b538367d772638948dd7f414b8658100194174a3
witalo <sousawitalo.gmail.com>
2020-02-27 22:11:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
+import Users from './user';
+import { Provider } from 'react-redux';
 
-function App() {
+import store from './store';
+
+const App = () => {
+  const [users, setUSers] = useState([
+    { id: 1, name: 'witalo' },
+    { id: 2, name: 'marcelo' },
+    { id: 3, name: 'carlos' },
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Users />
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'redux';
+
+const INITIAL_STATE = {
+    data: [
+        'node',
+        'java'
+    ],
+};
+
+function courses(state = INITIAL_STATE, action){
+    switch (action.type){
+        case 'ADD_COURSE':
+            return  { ...state, data: [...state.data, action.title ]};
+        default:
+            return state;    
+    }
+}
+
+const store = createStore(courses);
+
+export default store;
